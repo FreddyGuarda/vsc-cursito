2170053b2f33084765c0133309919674a920a061
Freddy Guarda <freddyguarda@hotmail.com>
2024-01-08 14:58:02 -0300

    Ultimo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Hola Freddy")
+function sumar(a,b) {
+    return a + b
+}
+
+let numero1 = 5;
+let numero2 = 2;
+
+let resultado = sumar(numero1, numero2)
+
+console.log("El resultado de sumar " + numero1 + " y " + "numero2" + "es: " + resultado)

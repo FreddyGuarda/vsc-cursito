60390c2ec0f6e9cb8e3c504595f7c6d364aa3c63
Freddy Guarda <freddyguarda@hotmail.com>
2024-01-09 12:44:12 -0300

    Ultimo repost

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function sumar(a,b) {
 let numero1 = 5;
 let numero2 = 2;
 
+
 let resultado = sumar(numero1, numero2)
 
-console.log("El resultado de sumar " + numero1 + " y " + "numero2" + "es: " + resultado)
+console.log("El resultado de sumar " + numero1 + " y " + numero2 + " " + "es: " + resultado)
